9616aad5430d7d7a967c5797312bf507a02a9aac
Jerry zhiya <jerryglitter399@gmail.com>
2026-03-19 08:04:36 +0100

    Updated image paths

--- my-portfolio/src/data.tsx
+++ my-portfolio/src/data.tsx
@@ -4,8 +4,8 @@ export const portfolioData = {
     name: "Adebanwo Mercy Temitope",
     title: "Executive Assistant & Operations Manager",
     bio: "I bring order to chaos. With 3 years of experience supporting C-level executives, I specialize in complex calendar management, travel logistics, and strategic operations. I don't just manage time; I optimize it.",
-    image: "/Mercy.png",
-    aboutImage: "/about-mercy.jpeg"
+    image: "/Adebanwo_Mercy/Mercy.png",
+    aboutImage: "/Adebanwo_Mercy/about-mercy.jpeg"
   },
   skills: [
     "Calendar Management", "Travel Logistics", "Event Planning", 
@@ -18,27 +18,27 @@ export const portfolioData = {
       text: "Mercy consistently demonstrates a proactive approach to her work. She is always ahead of the game, anticipating needs and taking initiative without waiting for direction.",
       name: "Reem Disu",
       role: "CEO, Disu Foundation",
-      image: "/Reem.jpg"
+      image: "/Adebanwo_Mercy/Reem.jpg"
     },
     {
       id: 2,
       text: "Mercy is a fast learner who takes initiative and anticipates tasks effectively. She is intentional about seeking clarity and ensures that every detail is handled to a high standard.”I hired Alexandra for a 3-month contract and she became indispensable. Her attention to detail in organizing our annual gala was flawless. Highly recommended.",
       name: "Rose Cruz",
       role: "CEO-Deputy, Disu Foundation",
-      image: "/Rose.png"
+      image: "/Adebanwo_Mercy/Rose.png"
     },
     {
       id: 3,
       text: "Mercy brings a high level of structure and foresight to her role. She is dependable, detail-oriented, and consistently ensures that operations run smoothly through her strong organizational and communication skills",
       name: "Kennedy Adetayo",
       role: "Country Manager, & Writer.",
-      image: "/Kennedy.JPG"
+      image: "/Adebanwo_Mercy/Kennedy.JPG"
     },
     {
       id: 4,
       text: "Mercy is highly organized and dependable. She consistently keeps me on track with timely reminders and follow-ups, while independently managing client communications and dealership partners with professionalism and efficiency.",
       name: "Nosa King Smart",
-      image: "/Nosa.PNG"
+      image: "/Adebanwo_Mercy/Nosa.PNG"
     }
   ]
 };
