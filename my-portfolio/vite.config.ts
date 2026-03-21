50a251cdf3a998862b6f156d67fe21009bfb2078
Jerry zhiya <jerryglitter399@gmail.com>
2026-03-21 10:50:56 +0100

    Edited base in config.js file

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Detect environment: Vercel sets process.env.VERCEL = "1"
 export default defineConfig({
   plugins: [react()],
-  base: '/Adebanwo_Mercy/'
+  base: process.env.VERCEL ? './' : '/Adebanwo_Mercy/',
 })
